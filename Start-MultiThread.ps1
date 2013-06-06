8ba53efa72924b06a43ad4fabbad3a6b3e9ece0c
Vidkun <vidkun.torvald@gmail.com>
2013-06-06 08:43:03 -0500

    major updates
    Updated README to reflect new features/funcitonality, added
    multithreading, rewrote a lot of things, cleaned up some code, added
    timestamps to output files, and more.

---
+++ Start-MultiThread.ps1
@@ -0,0 +1,34 @@
+﻿$machines = @("test1","test2","test3") # use this line to specify one or more specific target machines to enumerate
+# $machines = Get-Content .\hosts.txt # use this line to iterate through a text file list of target machines to enumerate
+
+# Get number of cores present and set max threads to double that.
+$numCores = gwmi -class win32_processor -Property "numberOfCores" | Select-Object -Property "numberOfCores"
+$maxThreads = $numCores.numberOfCores * 2
+$sleepTimer = 500
+$date = Get-Date -format yyyyMMdd_HHmm
+
+# Get current directory to pass along for proper output directory
+$invocation = (Get-Variable MyInvocation).Value
+$Dir = Split-Path $invocation.MyCommand.Path
+
+# Kill any existing jobs so they don't interfere with results
+"Killing existing jobs..."
+Get-Job | Remove-Job -Force
+"All jobs are dead!"
+
+$i = 0
+ForEach($ComputerName in $machines) {
+    # Check number of threads. If too many - take a nap
+    While ($(Get-Job -State running).count -ge $maxThreads) {
+        Write-Progress -Activity "Enumerating Groups" -Status "Waiting for threads to close" -CurrentOperation "$i threads created - $($(Get-Job -state running).count) threads open" -PercentComplete ($i / $machines.count * 100)
+        Start-Sleep -Milliseconds $sleepTimer
+    }
+    
+    # Start jobs 
+	i++
+    Start-Job {param($ComputerName,$date,$Dir); L:\Scripts\testing\groupEnum-mt.ps1 -host $ComputerName -date $date -directory $Dir } -ArgumentList $ComputerName,$date,$Dir -Name $ComputerName | Out-Null
+    Write-Progress -Activity "Enumerating Groups" -Status "Starting Threads" -CurrentOperation "$i threads created - $($(Get-Job -state running).count) threads open" -PercentComplete ($i / $machines.count * 100)
+    }
+     
+    Get-Job | Wait-Job
+    Get-Job | Receive-Job | Select-Object -ExcludeProperty RunspaceID | Export-csv $Dir\$date.wtf.csv -Append -NoTypeInformation -Force 
---
+++ groupEnum-mt.ps1
@@ -0,0 +1,238 @@
+[CmdletBinding(SupportsShouldProcess=$true)]
+<#
+.NAME
+    groupEmun.ps1
+.SYNOPSIS
+    Enumerate Windows group membership
+.DESCRIPTION
+    groupEnum will recursively enumerate the membership of any specified group, returning a list of users only (no groups). Results will be printed to the screen and written out to both text and csv files.
+.SYNTAX
+    groupEmun.ps1 [-d | -domain <domain name>] [-printName {true | false}] [-printSID {true | false}] [-printGroup {true | false}] $syntax += " [-printDisabled {true | false}] [-printPasswordAge {true | false}] {-g | -group} <group name> {-s | -source} <source file> {-r | -results} <results file> {-c | -csv} <CSV file>
+.PARAMETER d
+    domain of root group. Default: localhost
+.PARAMETER printName
+    {true|false} Display user name. Default: true
+.PARAMETER printSID
+    {true|false} Display user SID. Default: false
+.PARAMETER printGroup 
+    {true|false} Display inheritance chain. Default: false
+.PARAMETER printDisabled 
+    {true|false} Print whether or not the group is disabled. Default: true
+.PARAMETER printPasswordAge 
+    {true|false} Print age of user's password in days. Default: false
+.PARAMETER g -group -rootGroupName
+    <group name> group which to enumerate membership. Default: Administrators
+.PARAMETER s -source -sourceFile
+    <file name> source file of hosts to iterate through. Default: hosts.txt
+.PARAMETER r -results -resultsFile
+    <file name> text file to write out results to. Default: admins.txt
+.PARAMETER c -csv -csvFile
+    <file name> CSV file to write results to in CSV format. Default: admins.csv
+.PARAMETER h -host -ComputerName
+    <target host> Host machine to enumerate groups for Default: localhost
+.LINK
+    https://github.com/vidkun/groupEnum-mt
+.NOTES
+    Author: vidkun
+    Date: 20130603
+    Original code based on groupEnum from Doct0rZ (https://github.com/Doct0rZ/groupEnum)
+.EXAMPLE
+    .\groupEnum.ps1 -g "Administrators"
+    This will display all of the users that inherit permissions from local Administrators group
+#>
+
+#define parameters
+param(
+    [alias("d")]
+    [string]$domain           = $env:computername,
+    [string]$printName        = "true",
+    [string]$printSID         = "true",
+    [string]$printGroup       = "true",
+    [string]$printDisabled    = "true",
+    [string]$printPasswordAge = "true",
+    #[string]$date             = $(Get-Date -format yyyyMMdd_HHmm), # uncomment if running standalone
+    [string]$date             = "", # uncomment if running multithreaded
+    [alias("directory")][string]$Dir              = "",
+    #[alias("s","source")][string]$sourceFile = "$Dir\hosts.txt",
+    [alias("r","results")][string]$resultsFile = "$Dir\$date.admins.txt",
+    [alias("c","csv")][string]$csvFile = "$Dir\$date.admins.csv",
+    [Parameter(Position=0)][alias("h","host")][string]$ComputerName = "localhost",
+    [alias("g","group")]$rootGroupName = "Administrators"
+);
+
+# declare some variables
+[String]$FileName = "groupEnum-mt.ps1";    # name of the script file
+
+# Main function
+function global:main([string]$ComputerName) {
+ 
+    # check that group exists
+    if ($domain -eq $env:computername) {
+        $rootGroupObj = new-object System.Security.Principal.NTAccount($rootGroupName)
+	} else {
+        $rootGroupObj = new-object System.Security.Principal.NTAccount($domain, $rootGroupName)
+	}
+    [boolean]$isValid = isGroup($rootGroupObj)
+    if ( $isValid -eq $false ) {
+        $acct = $rootGroupObj.ToString()
+        $host.ui.WriteErrorLine("Error: $acct is not a valid account")
+        Exit
+	}
+           
+    # recursively get members
+    if ($rootGroupObj.ToString().Contains("\")) {
+        $rootName = $rootGroupObj.ToString().Split("\")[1]
+	} else {
+        $rootName = $rootGroupObj.ToString()
+	}
+    
+    #$ComputerName # for testing purposes
+    
+    $userTable = New-Object System.Collections.HashTable
+    $groupStack = New-Object System.Collections.Stack
+    $rootNtObj = @{ "ntobj" = $rootGroupObj;
+                    "name" = "\" + $rootName;
+                    "path" = "";
+                    "sid" = getSID($rootGroupObj);
+                    "domain" = $ComputerName
+	}
+    $groupStack.Push($rootNtObj)
+    while($groupStack.Count -gt 0) {
+        # get the current group info
+        $currGroup = $groupStack.Pop()
+        # set path for children
+        $path = $currGroup["path"] + $currGroup["name"] 
+        # Get members of group
+        $winnt = "WinNT://" + $currGroup["domain"] + "/" + $currGroup["name"] + ",group" 
+        $group = [ADSI]"$winnt"
+        $members = @($group.psbase.Invoke("Members"))
+        Write-Verbose ("Getting members of group: \" + $currGroup['domain'] + $currGroup['name'])
+        $members | foreach { 
+            $memName = $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)
+            $memdom = $_.GetType().InvokeMember("Parent", 'GetProperty', $null, $_, $null).split("/")[-1]
+            if ($memdom -eq $env:computername) {
+                $currObj = new-object System.Security.Principal.NTAccount($memName)
+	        } else {
+                $currObj = new-object System.Security.Principal.NTAccount($memdom, $memName)
+	        }
+
+            $sid = getSID($currObj)
+            $newObj = @{    "ntobj" = $currObj;
+                            "name" = "\" + $memName;
+                            "path" = $path;
+                            "sid" = $sid;
+                            "domain" = $memdom
+            }
+            # if member is a group, add it to the stack
+            if ( isGroup($currObj) ) {
+                Write-Verbose ("Found group: \" + $newObj['domain'] + $newObj['name'])
+                $groupStack.Push($newObj)
+            # if member is a user
+			} elseif ( isUser($currObj) ) {
+                Write-Verbose ("Found user: \" + $newObj['domain'] + $newObj['name'])
+                # check if user is disabled
+                $winnt = "WinNT://" + $newObj['domain'] +"/" + $newObj['name'] + ",user"
+                $newObj["disabled"] = ([ADSI]"$winnt").AccountDisabled
+                # get password age
+                $newObj["passwd_age"] = [math]::Round(([ADSI]"$winnt").InvokeGet('PasswordAge') / 86400 )
+                if ($userTable.Contains($newObj['sid']) -ne $true) {
+                    $userTable.Add($newObj['sid'], $newObj)
+				}
+			} elseif ($memdom -eq "NT AUTHORITY") {
+                Write-Verbose ("Found object: " + $newObj['domain'] + $newObj['name'])
+                $newObj["disabled"] = $false
+                $newObj["passwd_age"] = "-1"
+                if ($userTable.Contains($newObj['sid']) -ne $true) {
+                    $userTable.Add($newObj['sid'], $newObj)
+				}
+			}
+        }
+	}
+
+    # print users
+    $out = New-Object System.Collections.ArrayList
+    foreach ($user in $userTable.Values) {
+        $line = $ComputerName + ": " + "\" + $user['domain']
+        if ($printGroup -like "true") { $line += $user['path']}
+        if ($printName -like "true") { $line += $user['name']}
+        if ($printSID -like "true") { 
+            if ($printGroup -like "true" -or $printName -like "true") { $line += ',' }
+            $line += $user['sid']
+        }
+        if ($printPasswordAge -like "true") { $line += " (Password Age = " + $user["passwd_age"] + " days)" }
+        if (($printDisabled -like "true") -and $user["disabled"]) { $line += " [DISABLED]" }
+        [void]$out.Add($line)
+    }
+    
+    $out | Sort-Object
+    #$out | Sort-Object | Out-File $resultsFile -Append -Encoding utf8
+
+    $csvout = @()
+    foreach ($user in $userTable.Values) {
+        $output = "" | Select Computer,Account,Path,SID,Age,Disabled
+        $output.Computer = $ComputerName
+        $output.Account = $user["domain"] + $user["name"]
+        $output.Path = $user["path"]
+        $output.Disabled = $user["disabled"]
+        $output.SID = $user["sid"]
+        $output.Age = $user["passwd_age"]
+
+        $csvout =  $csvout + $output 
+           
+
+    }
+    $csvout | Export-Csv $csvFile -Append -NoTypeInformation -Force 
+    $csvout
+    
+    
+}
+
+
+
+# determine if NT Object is a group
+function global:isGroup([System.Security.Principal.NTAccount]$o) {
+    
+    ($d, $g) = $o.ToString().split("\")
+
+    if ($g -eq $null) {
+        $g = $d;
+        $d = $ENV:COMPUTERNAME;
+	}
+    try {
+        [boolean]$result = [System.DirectoryServices.DirectoryEntry]::Exists("WinNT://$d/$g,group")
+        return $result
+	} catch {
+        return $false
+	}
+}
+
+# determine if NT Object is a user
+function global:isUser([System.Security.Principal.NTAccount]$o) {
+    
+    ($d, $g) = $o.ToString().split("\")
+
+    if ($g -eq $null) {
+        $g = $d;
+        $d = $ENV:COMPUTERNAME;
+	}
+    try {
+        $result = [System.DirectoryServices.DirectoryEntry]::Exists("WinNT://$d/$g,user")
+        return $result
+	} catch {
+        return $false
+	}
+}
+
+# translate NTAccount object to SID
+function global:getSID([System.Security.Principal.NTAccount]$o) {
+    try {
+    $sid = $o.Translate([System.Security.Principal.SecurityIdentifier])
+	} catch { }
+    return $sid.value
+}
+
+# call main if script called directly
+if ($MyInvocation.MyCommand.Name -eq $FileName) {
+    main $ComputerName
+    Exit
+}
